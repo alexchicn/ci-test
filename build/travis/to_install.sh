b64e94edf3dc6be3c0cad04c57fb2100f61a76f4
Alex Chi <alex@alexchi.me>
2014-12-04 08:32:20 +0800

    update5

--- build/travis/to_install.sh
+++ build/travis/to_install.sh
@@ -11,14 +11,14 @@ if [ -z $TRAVIS_TAG ]; then
 if [ "$RUN_COVERALLS"x = "true"x ]; then
   if [ "$CXX" == "g++" ]; then sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test; fi
   sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
-  sudo apt-get update
+  sudo apt-get update -qq
   if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
   if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
   $CXX --version
   $CC --version
 #  sudo apt-get install aptitude
-  echo "-- install cppcheck valgrind coveralls"
-  sudo apt-get install cppcheck valgrind cpp-coveralls
+  #echo "-- install cppcheck valgrind coveralls"
+  #sudo apt-get install cppcheck valgrind cpp-coveralls
   #echo "-- install gles1"
   #sudo apt-get install libgles1-mesa-dev
   #echo "-- install qt5"
@@ -40,8 +40,8 @@ cmake --version
 #moc -v
 #echo "-- uic -v"
 #uic -v
-echo "-- valgrind --version"
-valgrind --version
+#echo "-- valgrind --version"
+#valgrind --version
 #echo "-- aptitude --version"
 #aptitude --version
 #echo "aptitude search gles"
