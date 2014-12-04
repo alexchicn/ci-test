5aa6188c6a217cebaf43ea2290748a6aee8543e0
Alex Chi <alex@alexchi.me>
2014-12-04 08:39:40 +0800

    update8

--- build/travis/to_install.sh
+++ build/travis/to_install.sh
@@ -9,7 +9,7 @@ set -e
 
 if [ -z $TRAVIS_TAG ]; then
 if [ "$RUN_COVERALLS"x = "true"x ]; then
-  if [ "$CXX" == "g++" ]; then sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test; fi
+  if [ "$CXX" == "g++" ]; then sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test; fi
   sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
   sudo apt-get update -qq
   if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
@@ -17,31 +17,34 @@ if [ "$RUN_COVERALLS"x = "true"x ]; then
   $CXX --version
   $CC --version
 #  sudo apt-get install aptitude
-  #echo "-- install cppcheck valgrind coveralls"
-  #sudo apt-get install cppcheck valgrind cpp-coveralls
-  #echo "-- install gles1"
-  #sudo apt-get install libgles1-mesa-dev
-  #echo "-- install qt5"
-  #sudo apt-get install qt5-default
+  echo "-- install cppcheck"
+  sudo apt-get install cppcheck
+  echo "-- install valgrind"
+  sudo apt-get install valgrind
+  echo "-- install gles1"
+  sudo apt-get install libgles1-mesa-dev
+  echo "-- install qt5"
+  sudo apt-get install qt5-default
+  echo "-- install coveralls"
+  sudo pip install cpp-coveralls
 fi
 else
   echo "! no install for coveralls"
-  exit
 fi
 
 # test some applications
-#echo "-- cmake --version"
-#cmake --version
-#echo "-- ls qt"
-#ls -l /usr/include/ | grep qt
-#echo "-- ls gl"
-#ls -l /usr/include/ | grep GL
-#echo "-- moc -v"
-#moc -v
-#echo "-- uic -v"
-#uic -v
-#echo "-- valgrind --version"
-#valgrind --version
+echo "-- cmake --version"
+cmake --version
+echo "-- ls qt"
+ls -l /usr/include/ | grep qt
+echo "-- ls gl"
+ls -l /usr/include/ | grep GL
+echo "-- moc -v"
+moc -v
+echo "-- uic -v"
+uic -v
+echo "-- valgrind --version"
+valgrind --version
 #echo "-- aptitude --version"
 #aptitude --version
 #echo "aptitude search gles"
