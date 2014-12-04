22433741b68d639e35776d8f648ec2246c4d5cc0
Alex Chi <alex@alexchi.me>
2014-12-04 08:20:32 +0800

    update2

--- build/travis/to_install.sh
+++ build/travis/to_install.sh
@@ -9,8 +9,13 @@ set -e
 
 if [ -z $TRAVIS_TAG ]; then
 if [ "$RUN_COVERALLS"x = "true"x ]; then
+  if [ "$CXX" == "g++" ]; then sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test; fi
   sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
   sudo apt-get update
+  if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
+  if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
+  $CXX --version
+  $CC --version
 #  sudo apt-get install aptitude
   echo "-- install cppcheck"
   sudo apt-get install cppcheck
@@ -22,12 +27,6 @@ if [ "$RUN_COVERALLS"x = "true"x ]; then
   sudo apt-get install qt5-default
   echo "-- install coveralls"
   sudo pip install cpp-coveralls
-  if [ "$CXX" == "g++" ]; then sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test; fi
-  sudo apt-get update
-  if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
-  if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
-  $CXX --version
-  $CC --version
 fi
 else
   echo "! no install for coveralls"
