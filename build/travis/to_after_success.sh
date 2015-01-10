d5bf2226a959e1dcc6afa17fba5acc50d0be0178
Alex Chi <alex@alexchi.me>
2015-01-10 08:39:58 +0800

    update

--- build/travis/to_after_success.sh
+++ build/travis/to_after_success.sh
@@ -4,6 +4,7 @@ echo ">>> after success"
 
 if [ -z $TRAVIS_TAG ]; then
 if [ "$RUN_COVERALLS"x = "true"x ]; then
+  sudo ln -s /usr/bin/gcov-4.8 /usr/bin/gcov
   ./out/bin/demo
   coveralls --exclude build/ --gcov-options '\-lp'
 fi
